cc17b2bb6014e6131a8945f26a6ae2e83961b229
lokiwonder <loki@wondermove.net>
2024-06-23 18:08:18 +0900

    msa

---
+++ apps/reservations/src/reservations/reservations.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
+import { DatabaseModule, LoggerModule } from '@app/common';
+import { ReservationsRepository } from './reservations.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './entities/reservation.entity';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(), //we wil setup this env later
+      }),
+    }),
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService, ReservationsRepository],
+})
+export class ReservationsModule {}
